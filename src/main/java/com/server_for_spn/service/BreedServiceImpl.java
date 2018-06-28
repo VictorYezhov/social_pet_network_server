b031a4b5452508154de595474628b3e796902ba7
VictorYezhov <vicyezh@gmail.com>
2018-06-28 19:09:58 +0300

    breedService

--- src/main/java/com/server_for_spn/service/BreedServiceImpl.java
+++ src/main/java/com/server_for_spn/service/BreedServiceImpl.java
@@ -1,17 +1,44 @@
 package com.server_for_spn.service;
 
 import com.server_for_spn.dao.BreedDAO;
+import com.server_for_spn.entity.Breed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Victor on 28.06.2018.
  */
 @Service
-public class BreedServiceImpl {
+public class BreedServiceImpl implements BreedService {
 
     @Autowired
-    BreedDAO breedDAO;
+    private BreedDAO breedDAO;
 
 
+    @Override
+    public void save(Breed breed) {
+        breedDAO.save(breed);
+    }
+
+    @Override
+    public List<Breed> findAll() {
+        return breedDAO.findAll();
+    }
+
+    @Override
+    public Breed findOne(Long id) {
+        return breedDAO.getOne(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        breedDAO.deleteById(id);
+    }
+
+    @Override
+    public void update(Breed breed) {
+        breedDAO.save(breed);
+    }
 }
