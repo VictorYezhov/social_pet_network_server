8b79847a71da9c6884e857e8ddfcbeadd986f7a8
MaxKomarenski <komarenski@ucu.edu.ua>
2018-06-28 17:56:28 +0300

    connection between country, city and user

---
+++ src/main/java/com/server_for_spn/entity/City.java
@@ -0,0 +1,56 @@
+package com.server_for_spn.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.List;
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    @JsonManagedReference
+    private Country country;
+
+    @OneToMany(mappedBy = "city", fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
