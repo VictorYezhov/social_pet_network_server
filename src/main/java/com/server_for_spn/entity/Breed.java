c24cf1a79bb1e76f955db953ab4e3e14a72dde3a
VictorYezhov <vicyezh@gmail.com>
2018-06-28 17:57:22 +0300

    Breed + pet

--- src/main/java/com/server_for_spn/entity/Breed.java
+++ src/main/java/com/server_for_spn/entity/Breed.java
@@ -3,25 +3,23 @@ package com.server_for_spn.entity;
 import com.server_for_spn.enums.PetType;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by Victor on 28.06.2018.
  */
 @Entity
-public class Pet {
+public class Breed {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated
+    private PetType type;
 
     private String name;
-    private int age;
 
-    @ManyToOne
-    @JoinColumn(name = "breed_id")
-    private Breed breed;
-
-
-
+    @OneToMany(mappedBy = "breed", fetch = FetchType.LAZY)
+    private List<Pet> petList;
 
 }
