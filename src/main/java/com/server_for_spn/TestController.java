cedbc348ef942ce4d5756876c6bd39ed96ccbe36
VictorYezhov <vicyezh@gmail.com>
2018-06-28 17:25:58 +0300

    Initial commit

---
+++ src/main/java/com/server_for_spn/TestController.java
@@ -0,0 +1,18 @@
+package com.server_for_spn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Victor on 28.06.2018.
+ */
+@RestController
+public class TestController {
+
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "test\n";
+    }
+}
---
+++ src/test/java/com/server_for_spn/SocialPetNetworkServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.server_for_spn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialPetNetworkServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
